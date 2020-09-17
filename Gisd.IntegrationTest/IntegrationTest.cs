9df116c26f3159d8b7286a310fc8a4e564633dbe
rockd <vampiredd@live.com>
2020-09-17 03:44:07 -0500

    * IntegrationTest.cs:
    * Gisd.sln: Integration Test

---
+++ Gisd.IntegrationTest/IntegrationTest.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Gisd.AppService;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace Gisd.IntegrationTest
+{
+    public class IntegrationTest
+    {
+        protected readonly HttpClient _testClient;
+
+        public IntegrationTest()
+        {
+            var appFactory = new WebApplicationFactory<Startup>();
+            _testClient = appFactory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Test01()
+        {
+            var result = await _testClient.GetAsync("sieapi");
+        }
+    }
+}
